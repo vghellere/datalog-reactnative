e835696b0550951157c77fd04f01b89aff72cc3f
Vinicius Ghellere Sbardelotto <viniciusghellere@gmail.com>
2024-10-21 13:32:26 -0300

    feat: add `graphql-codegen` (#3)
    * chore: add graphql-codegen package
    
    * feat: add getTemperatureSamples query
    
    * chore: auto-generate graphql types
    
    * refactor: use the typed graphql query

---
+++ src/__generated__/graphql.ts
@@ -0,0 +1,35 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  /** An ISO 8601-encoded datetime */
+  ISO8601DateTime: { input: any; output: any; }
+};
+
+/** Attributes for creating a TemperatureSample */
+export type CreateTemperatureSampleInput = {
+  eventTimestamp: Scalars['ISO8601DateTime']['input'];
+  value: Scalars['Float']['input'];
+};
+
+export type GetTemperatureSamplesQueryVariables = Exact<{
+  nSamples: Scalars['Int']['input'];
+}>;
+
+
+export type GetTemperatureSamplesQuery = { __typename?: 'Query', temperatureSamples: Array<{ __typename?: 'TemperatureSample', id: string, value: number, eventTimestamp: any }> };
+
+
+export const GetTemperatureSamplesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetTemperatureSamples"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"nSamples"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"temperatureSamples"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"nSamples"},"value":{"kind":"Variable","name":{"kind":"Name","value":"nSamples"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"value"}},{"kind":"Field","name":{"kind":"Name","value":"eventTimestamp"}}]}}]}}]} as unknown as DocumentNode<GetTemperatureSamplesQuery, GetTemperatureSamplesQueryVariables>;
