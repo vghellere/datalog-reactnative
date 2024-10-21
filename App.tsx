1ed9465e1570875b7799f54d3f8677a1f2a65182
Vinicius Ghellere Sbardelotto <viniciusghellere@gmail.com>
2024-10-21 12:36:27 -0300

    feat: create `TemperatureHistory` page (#2)
    * chore: add apollo client and canvas chart
    
    * chore: add react navigation
    
    * feat: use NavigationContainer
    
    * chore: remove native canvas chart lib
    
    * chore: add apollo-link-timeout package
    
    * feat: implement TemperatureHistory page
    
    * feat: add Apollo client context

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+import ApolloLinkTimeout from "apollo-link-timeout";
+import Constants from "expo-constants";
+
+import TemperatureHistory from "./src/TemperatureHistory";
+
+const Stack = createNativeStackNavigator();
+const cache = new InMemoryCache();
+
+const graphqlHost =
+  Constants.expoConfig?.hostUri?.split(":").shift()?.concat(":3001") ??
+  "productionapi.com";
+
+const timeoutLink = new ApolloLinkTimeout(5000);
+const httpLink = new HttpLink({ uri: `http://${graphqlHost}/graphql` });
+const timeoutHttpLink = timeoutLink.concat(httpLink);
+
+const client = new ApolloClient({
+  link: timeoutHttpLink,
+  cache,
+  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="TemperatureHistory">
+          <Stack.Screen
+            name="TemperatureHistory"
+            component={TemperatureHistory}
+            options={{ title: `📈 Temperature History` }}
+          />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
